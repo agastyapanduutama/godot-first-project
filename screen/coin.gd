141d914dedcb7fdc18c5e25f9155655c4c53e585
agastyapanduutama <agastypandu@gmail.com>
2026-06-24 21:58:05 +0700

    first game

---
+++ screen/coin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	print(" i have been touched by player") # Replace with function body.
+	#queue_free()
+	animation_player.play("pickup_coin")
+	
--- script/coin.gd
+++ script/coin.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
 	print(" i have been touched by player") # Replace with function body.
-	queue_free()
+	animation_player.play("pickup_coin")
+	#queue_free()
+	#print("run pickup")
+	
